78fb859218a4318d9242bf685f3e8d31fd007e9b
Davi Ribeiro <davibens4@hotmail.com>
2022-01-18 21:22:08 -0300

    feat(jwt) implements protected route with jwt

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,6 +1,8 @@
+import { WithJwt, User } from '@app/modules/auth/guards/jwt.guard';
 import { parserClient } from '@app/modules/client/parses/parserClient';
 import { CreateClientValidatorPipe } from '@app/modules/client/pipes/create-client.pipe';
 import { Parser } from '@app/shared/decorators/parser.decorator';
+import { PayloadToken } from '@core/services/auth.service';
 import { ClientService } from '@core/services/client.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
@@ -14,6 +16,7 @@ export class ClientController {
         return this.clientService.createClient(data);
     }
 
+    @WithJwt()
     @Get()
     @Parser(parserClient)
     async getAllClients() {
